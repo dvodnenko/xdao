da9e530c085972a5b41b718a80e0781f7b4de401
dvodnenko <thekovalskii@gmail.com>
2025-07-23 17:18:20 +0300

    feat: add user model
    create user model in domain

---
+++ src/domain/user/models.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+
+@dataclass(eq=False, kw_only=True)
+class User:
+
+    id: int
+    name: str
+    email: str
+
+    def __eq__(self, other):
+        return isinstance(other, User) and self.id == other.id
