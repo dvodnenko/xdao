00fb730263d8c99956e06a8a85d755198ad0b8dd
dvodnenko <thekovalskii@gmail.com>
2025-07-23 17:25:15 +0300

    feat: add user services
    created user services class in domain

---
+++ src/domain/user/services.py
@@ -0,0 +1,21 @@
+from domain.user.models import User
+from domain.user.interfaces import UserDAO
+
+
+class UserService:
+    def __init__(self, dao: UserDAO):
+        self.dao = dao
+
+    def register_user(self, name: str, email: str) -> User:
+        if self.dao.exists_by_email(email):
+            raise ValueError('Email already registered')
+
+        new_user = User(id=0, name=name, email=email)
+        self.dao.save(new_user)
+        return new_user
+
+    def get_profile(self, user_id: int) -> User:
+        user = self.dao.get_by_id(user_id)
+        if not user:
+            raise ValueError('User not found')
+        return user
